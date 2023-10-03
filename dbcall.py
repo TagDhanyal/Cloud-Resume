a705a8bba6b24f68a6a613e4b29ce8236bd0765b
TagDhanyal <dhanyaltag716@gmail.com>
2023-10-03 15:53:49 +0200

    updated function code

--- dbcall.py
+++ dbcall.py
@@ -1,31 +1,39 @@
 import boto3
 import os
 
-def lambda_handler(event, context):
-    
-    TABLE_NAME = "counter"
-    db_client = boto3.client('dynamodb')
-    dynamodb = boto3.resource('dynamodb')
-    table = dynamodb.Table(TABLE_NAME)
+# Define the table name
+TABLE_NAME = os.environ.get("DYNAMODB_TABLE_NAME", "counter")
 
-    update = db_client.update_item(
+# Initialize the DynamoDB client and resource
+db_client = boto3.client('dynamodb')
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table(TABLE_NAME)
+
+def lambda_handler(event, context):
+    try:
+        # Attempt to retrieve the view count from DynamoDB
+        item = table.get_item(Key={"id": 0}).get("Item")
+        viewrcount = item.get("viewrcount", 0)
+    except KeyError:
+        # Handle the case where the "viewrcount" attribute does not exist
+        viewrcount = 0
+
+    # Increment the view count
+    db_client.update_item(
         TableName=TABLE_NAME,
         Key={"id": {"N": "0"}},
-        UpdateExpression="ADD visitcount :inc",
+        UpdateExpression="ADD viewrcount :inc",
         ExpressionAttributeValues={":inc": {"N": "1"}}
     )
 
-    getItems = table.get_item(Key={"id": 0})
-    itemsObjectOnly = getItems["Item"]
-    visitcount = itemsObjectOnly["visitcount"]
-
+    # Prepare the response
     response = {
         "headers": {
-            "content-type" : "application/json"
+            "content-type": "application/json"
         },
         "status_code": 200,
-        "body" : {
-            "count": visitcount
+        "body": {
+            "count": viewrcount
         }
     }
 
